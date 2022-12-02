c68c1da07dab21e5aba1f871d539548054658a64
JihadSalameh <IMOgh3003>
2022-12-02 21:48:02 +0200

    removing biometric authentication

--- app/src/main/java/com/example/snare/LoginActivity.java
+++ app/src/main/java/com/example/snare/LoginActivity.java
@@ -1,24 +1,14 @@
 package com.example.snare;
 
-import static androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG;
-import static androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.biometric.BiometricManager;
-import androidx.biometric.BiometricPrompt;
-import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -34,29 +24,19 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GetTokenResult;
 import com.google.firebase.auth.GoogleAuthProvider;
-
-import java.util.concurrent.Executor;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 100;
-    private static final int REQUEST_CODE = 200;
 
     private EditText email;
     private EditText password;
-    private ImageView biometricImg;
-
-    private Executor executor;
-    private BiometricPrompt biometricPrompt;
-    private BiometricPrompt.PromptInfo promptInfo;
 
     GoogleSignInClient mGoogleSignInClient;
 
     private FirebaseAuth auth;
     private FirebaseUser user;
-    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,14 +45,13 @@ public class LoginActivity extends AppCompatActivity {
 
         email = findViewById(R.id.email_Txt);
         password = findViewById(R.id.password_Txt);
-        biometricImg = findViewById(R.id.biometric_Auth);
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
-        sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
 
         if(user != null) {
-            biometricImg.setVisibility(View.VISIBLE);
+            startActivity(new Intent(LoginActivity.this, NotesActivity.class));
+            finish();
         }
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -80,55 +59,6 @@ public class LoginActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        BiometricManager biometricManager = BiometricManager.from(this);
-        switch (biometricManager.canAuthenticate(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)) {
-            case BiometricManager.BIOMETRIC_SUCCESS:
-                Toast.makeText(this, "App Can Authenticate Using Biometrics!", Toast.LENGTH_SHORT).show();
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
-                Toast.makeText(this, "No Biometric Sensors!", Toast.LENGTH_SHORT).show();
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
-                Toast.makeText(this, "Sensor Not Available!", Toast.LENGTH_SHORT).show();
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
-                // Prompts the user to create credentials that your app accepts.
-                final Intent enrollIntent = new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
-                enrollIntent.putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
-                        BIOMETRIC_STRONG | DEVICE_CREDENTIAL);
-                startActivityForResult(enrollIntent, REQUEST_CODE);
-                break;
-        }
-
-        executor = ContextCompat.getMainExecutor(this);
-        biometricPrompt = new BiometricPrompt(LoginActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
-            @Override
-            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
-                Toast.makeText(getApplicationContext(), "Authentication error: " + errString, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-                String email = sharedPreferences.getString("email", "");
-                String password = sharedPreferences.getString("password", "");
-                LoginUser(email, password);
-            }
-
-            @Override
-            public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
-                Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Biometric login for my app")
-                .setSubtitle("Log in using your biometric credential")
-                .setNegativeButtonText("Use account password")
-                .build();
     }
 
     private void signInGoogle() {
@@ -160,14 +90,6 @@ public class LoginActivity extends AppCompatActivity {
                         if(task.isSuccessful()) {
                             FirebaseUser user = auth.getCurrentUser();
 
-                            /*
-                            GETS EVERYTHING FROM GOOGLE AUTH WITHOUT THE DOB
-                            System.out.println(user.getPhotoUrl());
-                            System.out.println(user.getEmail());
-                            System.out.println(user.getDisplayName());
-                            System.out.println(user.getUid());
-                            */
-
                             Toast.makeText(LoginActivity.this, user.getEmail(), Toast.LENGTH_SHORT).show();
                             updateUI(user);
                         } else {
@@ -187,11 +109,6 @@ public class LoginActivity extends AppCompatActivity {
         auth.signInWithEmailAndPassword(email_txt, password_txt).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
             public void onSuccess(AuthResult authResult) {
-                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
-                editor.putString("email", email_txt);
-                editor.putString("password", password_txt);
-                editor.apply();
-
                 Toast.makeText(LoginActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(LoginActivity.this, NotesActivity.class));
                 finish();
@@ -219,10 +136,6 @@ public class LoginActivity extends AppCompatActivity {
 
     public void GoogleSignin(View view) {
         signInGoogle();
-    }
-
-    public void BiometricAuth(View view) {
-        biometricPrompt.authenticate(promptInfo);
     }
 
     public void GoToRegister(View view) {
